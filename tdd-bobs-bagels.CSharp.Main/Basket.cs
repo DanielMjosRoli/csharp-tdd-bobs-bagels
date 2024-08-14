09a3cf86d827808baa50b0dc842d557a491539db
DanielMjosRoli <daniel.mrfush@gmail.com>
2024-08-14 14:34:42 +0200

    All tests pass Daniel Mjøs Røli

--- tdd-bobs-bagels.CSharp.Main/Basket.cs
+++ tdd-bobs-bagels.CSharp.Main/Basket.cs
@@ -9,21 +9,46 @@ namespace tdd_bobs_bagels.CSharp.Main
     public class Basket
     {
         private int _capacity;
+        private List<string> bagels;
 
-        public Basket(int capacity) => this._capacity = capacity;
+        public Basket(int capacity)
+        {
+            this._capacity = capacity;
+            bagels = new List<string>();
+        }
+
+        private bool checkCapacity()
+        {
+            return bagels.Count < _capacity;
+        }
 
         public bool Add(string name)
         {
+            if (checkCapacity())
+            {
+                bagels.Add(name);
+                return true;
+            }
             return false;
         }
 
         public bool changeCapacity(int new_capasity)
         {
+            if(_capacity != new_capasity)
+            {
+                _capacity = new_capasity;
+                return true;
+            }
             return false;
         }
 
         public bool Remove(string name)
         {
+            if (bagels.Contains(name))
+            {
+                bagels.Remove(name);
+                return true;
+            }
             return false;
         }
     }
